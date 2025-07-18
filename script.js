e44ac4bbf46b2bf67c4799dbb85c67138a8e73b1
Victoria Ferry <ferryvictoria@hotmail.fr>
2025-07-18 14:21:24 +0200

    Ajout de la fonction isValidate

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 function isValidate(date){
-
+    let day = parseInt(date.substr(0, 2))
+    let month = parseInt(date.substr(3,2))
+    let year = parseInt(date.substr(6, 4))
+    if (year > 9999 || year < 999){
+        console.log(`L'année n'est pas valide. Merci de mettre une année comprise entre 1000 et 9999.`)
+    } else {
+        if (month > 13 || month < 0){
+            console.log(`Le mois n'est pas valide. Merci de mettre un mois comprise entre 01 et 12.`)
+        } else {
+            if (day < 0){
+                console.log(`Le jour n'est pas valide. Merci de mettre un jour positif.`)
+            } else { 
+                if (maxDaysInMonth(date) == true ) {
+                    console.log(`La date ${date} est valide`)
+                } else {
+                    console.log(`La date ${date} est invalide`)  
+                }
+            }
+        }
+    }
 }
 
-let date = 23/12/2022
+let date = "22/12/2024"
 
 //Vérification de la validité de la date selon le mois
 function maxDaysInMonth(date){
@@ -15,6 +34,7 @@ function maxDaysInMonth(date){
         if (month == month31Days[i]){
             if (day > 31){
                 console.log(`Il n'y a que 31 jours dans ce mois, merci d'entrer une date correcte.`)
+                return false
             }
         }
     }
@@ -22,6 +42,7 @@ function maxDaysInMonth(date){
         if (month == month30Days[i]){
             if (day > 30){
                 console.log(`Il n'y a que 30 jours dans ce mois, merci d'entrer une date correcte.`)
+                return false
             }
         }
     }
@@ -31,23 +52,27 @@ function maxDaysInMonth(date){
                 if (year % 400 == 0){
                     if (day > 29){
                         console.log(`Il n'y a que 29 jours dans ce mois, merci d'entrer une date correcte.`)
+                        return false
                     }
                 } else {
                     if (day > 28){
                         console.log(`Il n'y a que 28 jours dans ce mois, merci d'entrer une date correcte.`)
+                        return false
                     }
                 }    
             } else {
                 if (day > 29){
                         console.log(`Il n'y a que 29 jours dans ce mois, merci d'entrer une date correcte.`)
+                        return false
                 }
             } 
         } else {
             if (day > 28){
                     console.log(`Il n'y a que 28 jours dans ce mois, merci d'entrer une date correcte.`)
+                    return false
                 }
         }
-    }
+    } return true
 }
 
-maxDaysInMonth("29/02/2024")
+isValidate(date)
